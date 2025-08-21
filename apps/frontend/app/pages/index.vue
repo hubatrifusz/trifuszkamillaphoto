f0ff0921d17c1a63731ae5251fac7e1e02546e20
Huba Trifusz <huba.trifusz@gmail.com>
2025-08-21 10:38:09 +0200

    Refactor code structure for improved readability and maintainability

--- apps/frontend/app/pages/index.vue
+++ apps/frontend/app/pages/index.vue
@@ -1,7 +1,19 @@
 <script lang="ts"></script>
 
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <UButton color="secondary" variant="outline">asd</UButton>
+    <section id="hero" class="w-full flex">
+      <h1>
+        Őrizd meg kedvenced legszebb pillanatait – időtálló, professzionális
+        fotókon.
+      </h1>
+    </section>
+  </div>
 </template>
+
+<style scoped>
+#hero {
+  height: 100vh;
+}
+</style>
--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 import tailwindcss from "@tailwindcss/vite";
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   vite: { plugins: [tailwindcss()] },
+  css: ["@/assets/css/main.css"],
+  modules: ["@nuxt/ui"],
 });
