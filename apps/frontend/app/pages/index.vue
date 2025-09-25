99adcc16b21477247181ca3175876ded605fc349
Huba Trifusz <huba.trifusz@gmail.com>
2025-09-25 21:55:59 +0200

    style: update color variables in main.css for consistency and clarity; modify button color in index.vue

--- apps/frontend/app/pages/index.vue
+++ apps/frontend/app/pages/index.vue
@@ -2,16 +2,14 @@
 
 <template>
   <div>
-    <UButton color="secondary" variant="outline">asd</UButton>
+    <UButton color="primary">asd</UButton>
     <section id="hero" class="w-full flex">
       <h1>
         Őrizd meg kedvenced legszebb pillanatait – időtálló, professzionális
         fotókon.
       </h1>
     </section>
-    <section id="images" class="w-full columns-4">
-
-    </section>
+    <section id="images" class="w-full columns-4"></section>
   </div>
 </template>
 
